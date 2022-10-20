4885a28b10ca7d3eb33cec55f75d8ed2f02c63b0
kumoriheart <107945296+kumoriheart@users.noreply.github.com>
2022-10-20 12:56:26 +0200

    triayejkflsdf

---
+++ SpicyApp/SpicyAppApp.swift
@@ -0,0 +1,19 @@
+//
+//  SpicyAppApp.swift
+//  SpicyApp
+//
+//  Created by Lara on 17/10/22.
+//
+
+import SwiftUI
+
+@main
+struct SpicyAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+                ContentView()
+                
+            }
+        }
+    }
+
