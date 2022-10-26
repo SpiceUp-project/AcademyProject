1ded1bddc223ed2666cd081549007e2a6dc2c05b
mariavittorialabarbera <mlabarbera22@fed.idserve.net>
2022-10-26 12:00:14 +0200

    I did changes in the ModalView and Challenge Model to add description and tips

--- SpicyApp/Views/GivingUpView.swift
+++ SpicyApp/Views/GivingUpView.swift
@@ -14,7 +14,7 @@ struct GivingUpView: View {
             Spacer()
             
             Image("givingupImage")
-                .resizable(capInsets: EdgeInsets(top: 0.0, leading: 0.0, bottom: 0.0, trailing: 0.0))
+                .resizable()
                 .aspectRatio(contentMode: .fit)
             
             
